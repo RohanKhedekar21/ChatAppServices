884071e1ada54d580111769afbfedff8508a2b35
Rohan Khedekar <Rohan.khedekar21@gmail.com>
2024-08-27 21:53:32 +0530

    EC2 Configure

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,101 +1,97 @@
-const User = require("../model/userModel")
-const brcypt = require('bcrypt');
+const User = require("../model/userModel");
+const brcypt = require("bcrypt");
 
 module.exports.register = async (req, res, next) => {
-    console.info("Service Register call")
-    try {
+  console.info("Service Register call");
+  try {
+    const { username, email, password } = req.body;
 
-        const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
 
-        const usernameCheck = await User.findOne({ username })
-
-        if (usernameCheck) {
-            return res.json({ msg: "Username already used", status: false })
-        }
-
-        const emailCheck = await User.findOne({ email })
-
-        if (emailCheck) {
-            return res.json({ msg: "Email already used", status: false })
-        }
-
-        const hashedPassword = await brcypt.hash(password, 10);
-        const user = await User.create({
-            email,
-            username,
-            password: hashedPassword,
-        });
-        delete user.password;
-        return res.json({ status: true, user })
-
-    } catch (e) {
-        console.info("Error in service Register", e);
-        next(e)
+    if (usernameCheck) {
+      return res.json({ msg: "Username already used", status: false });
     }
 
-}
+    const emailCheck = await User.findOne({ email });
+
+    if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+    }
+
+    const hashedPassword = await brcypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (e) {
+    console.info("Error in service Register", e);
+    next(e);
+  }
+};
 
 module.exports.login = async (req, res, next) => {
-    console.info("Service Login call")
-    try {
+  console.info("Service Login call");
+  try {
+    const { username, password } = req.body;
 
-        const { username, password } = req.body;
+    const user = await User.findOne({ username });
 
-        const user = await User.findOne({ username })
-
-
-        if (!user) {
-
-            return res.json({ msg: "Incorrect username or password", status: false })
-        }
-        const isPasswordValid = await brcypt.compare(password, user.password)
-        if (!isPasswordValid) {
-            return res.json({ msg: "Incorrect username or password", status: false })
-        }
-
-        delete user.password;
-
-
-        return res.json({ status: true, user })
-
-    } catch (e) {
-        console.info("Error in service login", e)
-        next(e)
+    if (!user) {
+      return res.json({ msg: "Incorrect username or password", status: false });
+    }
+    const isPasswordValid = await brcypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.json({ msg: "Incorrect username or password", status: false });
     }
 
-}
+    delete user.password;
+
+    return res.json({ status: true, user });
+  } catch (e) {
+    console.info("Error in service login", e);
+    next(e);
+  }
+};
 
 module.exports.setAvatar = async (req, res, next) => {
-    console.info("Service setAvatar call")
-    try {
-        const userId = req.params.id;
-        const avatarImage = req.body.image;
-        const userData = await User.findByIdAndUpdate(userId, {
-            isAvatarImageSet: true,
-            avatarImage,
-        }, { new: true })
+  console.info("Service setAvatar call");
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
 
-        return res.json({ isSet: userData.isAvatarImageSet, image: userData.avatarImage })
-    } catch (e) {
-        console.info("Error in service setAvatar", e)
-        next(e)
-    }
-
-}
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (e) {
+    console.info("Error in service setAvatar", e);
+    next(e);
+  }
+};
 
 module.exports.getAllUsers = async (req, res, next) => {
-    console.info("Service getAllUsers call")
-    try {
-        const users = await User.find({ _id: { $ne: req.params.id } }).select([
-            "email",
-            "username",
-            "avatarImage",
-            "_id",
-        ])
-        return res.json(users)
-    } catch (e) {
-        console.info("Error in service getAllUsers", e)
-        next(e)
-    }
-
-}
+  console.info("Service getAllUsers call");
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (e) {
+    console.info("Error in service getAllUsers", e);
+    next(e);
+  }
+};
--- index.js
+++ index.js
@@ -1,77 +1,80 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose")
-const userRoutes = require("./routes/userRoutes")
-const messageRoutes = require("./routes/messagesRoute")
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messagesRoute");
 
 const app = express();
-const socket = require("socket.io")
-require("dotenv").config({ path: __dirname + '/.env' });
+const socket = require("socket.io");
+require("dotenv").config({ path: __dirname + "/.env" });
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth", userRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("DB Connection Successfull")
-}).catch((err) => {
-    console.log(err.message)
-})
+  })
+  .then(() => {
+    console.log("DB Connection Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server Started on Port ${process.env.PORT}`)
-})
+  console.log(`Server Started on Port ${process.env.PORT}`);
+});
 
 const io = socket(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        // origin: "http://rohan-chat-app.s3-website.ap-south-1.amazonaws.com",
-        credentials: true,
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    origin: "http://schmooze-chat-app.s3-website.ap-south-1.amazonaws.com",
+    credentials: true,
+  },
+});
 
 global.onlineUsers = new Map();
 
-io.on('connection', (socket) => {
-    global.chatSocket = socket;
-    socket.on('add-user', (userId) => {
-        onlineUsers.set(userId, socket.id);
-    })
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
-    socket.on('send-msg', (data) => {
-        const sendUserSocket = onlineUsers.get(data.to);
-        if (sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-recieve", data.message);
-        }
-    })
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
 
-    socket.on('user:call', async ({ to, from, offer }) => {
-        console.log(">>>>user:call")
-        const recieverSocket = onlineUsers.get(to);
-        if (recieverSocket) {
-            console.log(">>>>recieverSocket", recieverSocket)
-            socket.to(recieverSocket).emit('incomming:call', { from: from, offer })
-        }
-    })
+  socket.on("user:call", async ({ to, from, offer }) => {
+    console.log(">>>>user:call");
+    const recieverSocket = onlineUsers.get(to);
+    if (recieverSocket) {
+      console.log(">>>>recieverSocket", recieverSocket);
+      socket.to(recieverSocket).emit("incomming:call", { from: from, offer });
+    }
+  });
 
-    socket.on("call:accepted", ({ to, ans }) => {
-        const recieverSocket = onlineUsers.get(to);
-        io.to(recieverSocket).emit("call:accepted", { from: socket.id, ans });
-    });
+  socket.on("call:accepted", ({ to, ans }) => {
+    const recieverSocket = onlineUsers.get(to);
+    io.to(recieverSocket).emit("call:accepted", { from: socket.id, ans });
+  });
 
-    socket.on("peer:nego:needed", ({ to, offer }) => {
-        console.log("peer:nego:needed", offer);
-        const recieverSocket = onlineUsers.get(to);
-        io.to(recieverSocket).emit("peer:nego:needed", { from: socket.id, offer });
-    });
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    const recieverSocket = onlineUsers.get(to);
+    io.to(recieverSocket).emit("peer:nego:needed", { from: socket.id, offer });
+  });
 
-    socket.on("peer:nego:done", ({ to, ans }) => {
-        console.log("peer:nego:done", ans);
-        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-    });
-})
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
